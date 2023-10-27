6638354f96d6238ae69d7970201945d3149c83a2
wesleutje <wesleutje@live.nl>
2023-10-27 07:48:39 +0200

    Updated imports again

--- src/components/preregister/preregistersection.tsx
+++ src/components/preregister/preregistersection.tsx
@@ -2,26 +2,29 @@ import { useEffect, useState } from "react";
 import { Preregister, getPreregisters } from "../../services/api";
 import List from "./list/list";
 import Form from "./form/form";
+import React from "react";
 
 const PreregisterSection = () => {
-    const [preregistersCollection, setPreregistersCollection] = useState<Partial<Preregister>[]>([]);
+  const [preregistersCollection, setPreregistersCollection] = useState<
+    Partial<Preregister>[]
+  >([]);
 
-    const addPreregister = (preregister: Partial<Preregister>) => {
-        setPreregistersCollection(current => [...current, preregister]);
-    }
+  const addPreregister = (preregister: Partial<Preregister>) => {
+    setPreregistersCollection((current) => [...current, preregister]);
+  };
 
-    useEffect(() => {
-        getPreregisters().then(preregisters => {
-            if(preregisters) setPreregistersCollection(preregisters);
-        });
-    }, []);
+  useEffect(() => {
+    getPreregisters().then((preregisters) => {
+      if (preregisters) setPreregistersCollection(preregisters);
+    });
+  }, []);
 
-    return(
-        <>
-            <List preregistersCollection={preregistersCollection} />
-            <Form addPreregister={addPreregister} />
-        </>
-    );
-}
+  return (
+    <>
+      <List preregistersCollection={preregistersCollection} />
+      <Form addPreregister={addPreregister} />
+    </>
+  );
+};
 
 export default PreregisterSection;
