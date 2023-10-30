8e5893d39eb7c2885204fa38859af229a5b0ab2d
MsZoezo <zoey@zoezo.dev>
2023-10-30 09:06:25 +0100

    Navbar styling setup

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,19 +1,15 @@
 import "./nav.css";
-import afriQExpressLogo from "./afriQExpress-01.png";
+import afriQExpressLogo from "./logo.svg";
 
 
 const Navigation = () => {
     return (
         <nav className="navigation">
-            <figure>
-                <a href='/'><img src={afriQExpressLogo} className="navigation__logo" alt="Brand Logo" /></a>
-            </figure>
-            <ul className="navigation__list">
-                <li><a href="#Over">About Us</a></li>
-                <li><a href="#Features">Features</a></li>
-                <li><a href="#Contact">Contact</a></li>
-                <li><a href='/Login'><button className='navigation__list--login'>preregister</button></a></li>
-            </ul>
+            <a href='/' className="navigation__logo"><img src={afriQExpressLogo} alt="Brand Logo" /></a>
+            <a href="#Over" className="navigation__item">About Us</a>
+            <a href="#Features" className="navigation__item">Features</a>
+            <a href="#Contact" className="navigation__item">Contact</a>
+            <a href='/Login' className="navigation__special">preregister</a>
         </nav>
     );
 }
