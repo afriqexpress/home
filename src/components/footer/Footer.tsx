c9122e28b3bba92888a47f8dfb42624ec7c6784a
Damian <70946678+DamianvdD@users.noreply.github.com>
2023-10-30 15:17:00 +0100

    Footer update and cards

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,21 @@
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return(
+    return (
         <section className="footer">
+            <div className="footer__contact--wrapper">
+                <p className="footer__p">Neem contact met ons op!</p>
+                <FontAwesomeIcon className="footer__icon" icon={faEnvelope} /><p className="footer__p">afriqexpress@gmail.com</p>
+                <FontAwesomeIcon className="footer__icon" icon={faPhone} /><p className="footer__p">(+228) 70559093</p>
+                <Link to="/AfriQExpress" className="footer__p">AfriQExpress</Link>
+                <Link to="/AfriQPay" className="footer__p">AfriQPay</Link>
+            </div>
             <div className="footer__text--wrapper">
-                <p className="footer__p">Cookies</p>
                 <p className="footer__p">Privacy</p>
                 <p className="footer__p">Conditions</p>
-                
             </div>
             <p className="footer__p">Powered By afriQExpress © 2022-2023</p>
         </section>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 import Nav from "../components/nav/nav";
 import Header from "../components/homeheader/Header";
 import Features from "../components/service/Features";
 import Footer from "../components/footer/Footer";
-import PreregisterSection from "../components/preregister/preregistersection";
 
 const Home = () => {
   return (
@@ -15,18 +12,13 @@ const Home = () => {
         { link: "#Contact", name: "Contact" },
         { link: "/Login", name: "Preregister", special: true },
       ]} />
-
       <Header
         bigTitle="What do we do?" 
         smallTitle="about us text"
         buttonTitle="Preregister Now!"
       />
-      {/* <Features />
+      <Features />
       <Footer />
-      <Link to="/AfriQExpress">AfriQExpress</Link>
-      <Link to="/AfriQPay">AfriQPay</Link>
-
-      <PreregisterSection />
     </>
   );
 };
