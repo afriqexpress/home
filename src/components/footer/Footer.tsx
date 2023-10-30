8eef7bbbd1cb49ce807d15998955279a22948be8
wesleutje <wesleutje@live.nl>
2023-10-30 16:36:14 +0100

    navbar responsive
    images van social media moeten nog geladen worden. en juiste text voor de links en actual links.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,102 @@
 import "./Footer.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return (
-        <section className="footer">
-            <div className="footer__contact--wrapper">
-                <p className="footer__p">Neem contact met ons op!</p>
-                <FontAwesomeIcon className="footer__icon" icon={faEnvelope} /><p className="footer__p">afriqexpress@gmail.com</p>
-                <FontAwesomeIcon className="footer__icon" icon={faPhone} /><p className="footer__p">(+228) 70559093</p>
-                <Link to="/AfriQExpress" className="footer__p">AfriQExpress</Link>
-                <Link to="/AfriQPay" className="footer__p">AfriQPay</Link>
+  return (
+    // <section className="footer">
+    //     <div className="footer__contact--wrapper">
+    //         <p className="footer__p">Neem contact met ons op!</p>
+    //         <FontAwesomeIcon className="footer__icon" icon={faEnvelope} /><p className="footer__p">afriqexpress@gmail.com</p>
+    //         <FontAwesomeIcon className="footer__icon" icon={faPhone} /><p className="footer__p">(+228) 70559093</p>
+    //         <Link to="/AfriQExpress" className="footer__p">AfriQExpress</Link>
+    //         <Link to="/AfriQPay" className="footer__p">AfriQPay</Link>
+    //     </div>
+    //     <div className="footer__text--wrapper">
+    //         <p className="footer__p">Privacy</p>
+    //         <p className="footer__p">Conditions</p>
+    //     </div>
+    //     <p className="footer__p">Powered By afriQExpress © 2022-2023</p>
+    // </section>
+    <div className="footer">
+      <div className="sb__footer section__padding">
+        <div className="sb__footer-links">
+          <div className="sb__footer-links_div">
+            <h4>Resources</h4>
+            <a href="/">
+              <p>Resource center</p>
+            </a>
+            <a href="/">
+              <p>Testimonial</p>
+            </a>
+            <a href="/">
+              <p>STV</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Resources</h4>
+            <a href="/">
+              <p>Resource center</p>
+            </a>
+            <a href="/">
+              <p>Testimonial</p>
+            </a>
+            <a href="/">
+              <p>STV</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Coming soon on</h4>
+            <div className="socialmedia">
+              <p>
+                <img src="" alt="" />
+              </p>
+              <p>
+                <img src="" alt="" />
+              </p>
+              <p>
+                <img src="" alt="" />
+              </p>
+              <p>
+                <img src="" alt="" />
+              </p>
             </div>
-            <div className="footer__text--wrapper">
-                <p className="footer__p">Privacy</p>
-                <p className="footer__p">Conditions</p>
-            </div>
-            <p className="footer__p">Powered By afriQExpress © 2022-2023</p>
-        </section>
-    );
-}
+          </div>
+        </div>
+        <hr></hr>
+        <div className="sb__footer-below">
+          <div className="sb__footer-copyright">
+            <p>
+              @{new Date().getFullYear()} AfriQExpress. All rights reserved.
+            </p>
+          </div>
+          <div className="sb__footer-below-links">
+            <a href="">
+              <div>
+                <p>Terms & Conditions</p>
+              </div>
+            </a>
+            <a href="">
+              <div>
+                <p>Privacy</p>
+              </div>
+            </a>
+            <a href="">
+              <div>
+                <p>Security</p>
+              </div>
+            </a>
+            <a href="">
+              <div>
+                <p>Cookies</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,17 @@
 import Nav from "../components/nav/nav";
-import Header from "../components/homeheader/Header";
 import Features from "../components/service/Features";
 import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <>
-      <Nav links={[
-        { link: "#Over", name: "About Us" },
-        { link: "#Features", name: "Features" },
-        { link: "#Contact", name: "Contact" },
-        { link: "/Login", name: "Preregister", special: true },
-      ]} />
-      <Header
-        bigTitle="What do we do?" 
-        smallTitle="about us text"
-        buttonTitle="Preregister Now!"
+      <Nav
+        links={[
+          { link: "#Over", name: "About Us" },
+          { link: "#Features", name: "Features" },
+          { link: "#Contact", name: "Contact" },
+          { link: "/Login", name: "Preregister", special: true },
+        ]}
       />
       <Features />
       <Footer />
