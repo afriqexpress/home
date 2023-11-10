37ebd5d1a87117ff4d3a373af088f40df3d98c89
Damian <70946678+DamianvdD@users.noreply.github.com>
2023-11-10 08:59:35 +0100

    fixed the feature cards

--- src/components/afriqexpresspage/features/features.tsx
+++ src/components/afriqexpresspage/features/features.tsx
@@ -5,8 +5,8 @@ const Features = () =>
     <>
         <h2 className="features__h2">Onze Features</h2>
         <div className="features__wrapper2">
-            {FeaturesData.map(feature => (
-                <section className="features" id="Features">
+            {FeaturesData.map((feature, index) => (
+                <section className="features" id={`Features${index}`} key={index}>
                     <div className="features__wrapper">
                         <img className="features__image" src={feature.image} alt="Prints" />
                         <h3 className="features__h3">{feature.title}</h3>
