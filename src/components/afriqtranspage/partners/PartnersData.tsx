cca04151e95e8d7a9f3907098d3d7015765cdc28
Damian <70946678+DamianvdD@users.noreply.github.com>
2023-12-14 14:02:26 +0100

    Added partner and fixed all the little bugs after merge of the redesign to main branch

--- src/components/afriqtranspage/partners/PartnersData.tsx
+++ src/components/afriqtranspage/partners/PartnersData.tsx
@@ -1,22 +1,22 @@
 export const PartnersData = [
     {
         id: 1,
-        image: "",
-        title: "Partner 1",
+        image: "/img/svg/partners/nad.svg",
+        title: "NAD Service",
     },
-    {
-        id: 2,
-        image: "",
-        title: "Partner 2",
-    },
-    {
-        id: 3,
-        image: "",
-        title: "Partner 3",
-    },
-    {
-        id: 4,
-        image: "",
-        title: "Partner 4",
-    },
+    // {
+    //     id: 2,
+    //     image: "",
+    //     title: "Partner 2",
+    // },
+    // {
+    //     id: 3,
+    //     image: "",
+    //     title: "Partner 3",
+    // },
+    // {
+    //     id: 4,
+    //     image: "",
+    //     title: "Partner 4",
+    // },
 ]
